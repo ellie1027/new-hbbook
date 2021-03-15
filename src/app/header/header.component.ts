68b99aedd352a535799c080e8302349aa500798f
hanbyul goo <beammeupplzz@gmail.com>
2021-03-16 00:13:52 +0900

    modify nav styles and functions

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ScullyRoutesService } from '@scullyio/ng-lib';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  all:number = 0;
 
-  constructor() { }
+  constructor(public scully : ScullyRoutesService) { }
 
   ngOnInit(): void {
+    this.scully.available$.subscribe(
+      scully => { 
+        this.all = scully.length - 1;
+      }
+    ); 
   }
 
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,32 +7,28 @@ import { ScullyRoutesService } from '@scullyio/ng-lib';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  nav:any = {};
-  folder:string = "";
+  folder:any = [];
+  posts:any = [];
 
   constructor(public scully : ScullyRoutesService) {
   }
 
   ngOnInit(): void {
-    this.list;
-  }
-
-  get list(){
-
     this.scully.available$.subscribe(
       scully => { 
         for(let route of scully){
           //루트 제외
           if(route.route.length != 1){
-            this.folder = route.route.split('/')[2];
-            if(this.nav[this.folder] == undefined) this.nav[this.folder] = [];
-            this.nav[this.folder].push(route.route.split('/')[3]);
+            let f = route.route.split('/')[2];
+
+            if(this.folder.indexOf(f) == -1) this.folder.push(f);
+            
+            if(this.posts[f] == undefined) this.posts[f] = [];
+            this.posts[f].push(route.route.split('/')[3]);
           }
         }   
       }
-    );  
-    console.log(this.nav);
-    return this.nav;
+    ); 
   }
 
 }
